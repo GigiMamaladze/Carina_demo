925d56695d0993150c45252c7790b2edbf72a29c
GigiMamaladze <gigimamaladze13@gmail.com>
2022-10-27 01:13:26 +0400

    uploaded carina-demo

---
+++ src/main/java/com/qaprosoft/carina/demo/utils/MobileContextUtils.java
@@ -0,0 +1,66 @@
+package com.qaprosoft.carina.demo.utils;
+
+import com.qaprosoft.carina.core.foundation.webdriver.DriverHelper;
+import com.qaprosoft.carina.core.foundation.webdriver.IDriverPool;
+//import io.appium.java_client.remote.SupportsContextSwitching;
+import org.openqa.selenium.ContextAware;
+import org.openqa.selenium.NotFoundException;
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.support.decorators.Decorated;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+import java.util.Set;
+
+
+public class MobileContextUtils implements IDriverPool {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    /**
+     * Returns a pure driver without listeners
+     */
+    public WebDriver getPureDriver(WebDriver driver) {
+//        if (driver instanceof Decorated<?>) {
+//            driver = (WebDriver) ((Decorated<?>) driver).getOriginal();
+//        }
+        return driver;
+    }
+
+    public void switchMobileContext(View context) {
+        WebDriver driver = getDriver();
+        DriverHelper help = new DriverHelper();
+        Set<String> contextHandles = help.performIgnoreException(((ContextAware) driver)::getContextHandles);
+        String desiredContext = "";
+        boolean isContextPresent = false;
+        LOGGER.info("Existing contexts: ");
+        for (String cont : contextHandles) {
+            if (cont.contains(context.getView())) {
+                desiredContext = cont;
+                isContextPresent = true;
+            }
+            LOGGER.info(cont);
+        }
+        if (!isContextPresent) {
+            throw new NotFoundException("Desired context is not present");
+        }
+        LOGGER.info("Switching to context : " + context.getView());
+//        ((SupportsContextSwitching) driver).context(desiredContext);
+    }
+
+    public enum View {
+        NATIVE("NATIVE_APP"),
+        WEB("WEBVIEW_");
+
+        String viewName;
+
+        View(String viewName) {
+            this.viewName = viewName;
+        }
+
+        public String getView() {
+            return this.viewName;
+        }
+    }
+}
---
+++ src/test/java/com/qaprosoft/carina/demo/WebSampleTest.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2013-2021 QAPROSOFT (http://qaprosoft.com/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.qaprosoft.carina.demo;
+
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import com.qaprosoft.carina.core.foundation.IAbstractTest;
+import com.zebrunner.agent.core.annotation.TestLabel;
+import com.qaprosoft.carina.core.foundation.utils.ownership.MethodOwner;
+import com.qaprosoft.carina.core.foundation.utils.tag.Priority;
+import com.qaprosoft.carina.core.foundation.utils.tag.TestPriority;
+import com.qaprosoft.carina.demo.gui.components.FooterMenu;
+import com.qaprosoft.carina.demo.gui.components.NewsItem;
+import com.qaprosoft.carina.demo.gui.components.compare.ModelSpecs;
+import com.qaprosoft.carina.demo.gui.components.compare.ModelSpecs.SpecType;
+import com.qaprosoft.carina.demo.gui.pages.BrandModelsPage;
+import com.qaprosoft.carina.demo.gui.pages.CompareModelsPage;
+import com.qaprosoft.carina.demo.gui.pages.HomePage;
+import com.qaprosoft.carina.demo.gui.pages.ModelInfoPage;
+import com.qaprosoft.carina.demo.gui.pages.NewsPage;
+
+/**
+ * This sample shows how create Web test.
+ *
+ * @author qpsdemo
+ */
+public class WebSampleTest implements IAbstractTest {
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    @TestPriority(Priority.P3)
+    @TestLabel(name = "feature", value = {"web", "regression"})
+    public void testModelSpecs() {
+        // Open GSM Arena home page and verify page is opened
+        HomePage homePage = new HomePage(getDriver());
+        homePage.open();
+        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
+        
+        //Closing advertising if it's displayed
+        homePage.getWeValuePrivacyAd().closeAdIfPresent();
+        
+        // Select phone brand
+        homePage = new HomePage(getDriver());
+        BrandModelsPage productsPage = homePage.selectBrand("Samsung");
+        // Select phone model
+        ModelInfoPage productInfoPage = productsPage.selectModel("Galaxy A52 5G");
+        // Verify phone specifications
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(productInfoPage.readDisplay(), "6.5\"", "Invalid display info!");
+        softAssert.assertEquals(productInfoPage.readCamera(), "64MP", "Invalid camera info!");
+        softAssert.assertEquals(productInfoPage.readRam(), "6/8GB RAM", "Invalid ram info!");
+        softAssert.assertEquals(productInfoPage.readBattery(), "4500mAh", "Invalid battery info!");
+        softAssert.assertAll();
+    }
+
+
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    @TestPriority(Priority.P1)
+    @TestLabel(name = "feature", value = {"web", "acceptance"})
+    public void testCompareModels() {
+        // Open GSM Arena home page and verify page is opened
+        HomePage homePage = new HomePage(getDriver());
+        homePage.open();
+        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
+        // Open model compare page
+        FooterMenu footerMenu = homePage.getFooterMenu();
+        Assert.assertTrue(footerMenu.isUIObjectPresent(2), "Footer menu wasn't found!");
+        CompareModelsPage comparePage = footerMenu.openComparePage();
+        // Compare 3 models
+        List<ModelSpecs> specs = comparePage.compareModels("Samsung Galaxy J3", "Samsung Galaxy J5", "Samsung Galaxy J7 Pro");
+        // Verify model announced dates
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(specs.get(0).readSpec(SpecType.ANNOUNCED), "2016, March 31");
+        softAssert.assertEquals(specs.get(1).readSpec(SpecType.ANNOUNCED), "2015, June 19");
+        softAssert.assertEquals(specs.get(2).readSpec(SpecType.ANNOUNCED), "2017, June");
+        softAssert.assertAll();
+    }
+    
+    @Test()
+    @MethodOwner(owner = "qpsdemo")
+    @TestLabel(name = "feature", value = {"web", "acceptance"})
+    public void testNewsSearch() {
+        HomePage homePage = new HomePage(getDriver());
+        homePage.open();
+        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened!");
+        
+        NewsPage newsPage = homePage.getFooterMenu().openNewsPage();
+        Assert.assertTrue(newsPage.isPageOpened(), "News page is not opened!");
+        
+        final String searchQ = "iphone";
+        List<NewsItem> news = newsPage.searchNews(searchQ);
+        Assert.assertFalse(CollectionUtils.isEmpty(news), "News not found!");
+        SoftAssert softAssert = new SoftAssert();
+        for(NewsItem n : news) {
+            System.out.println(n.readTitle());
+            softAssert.assertTrue(StringUtils.containsIgnoreCase(n.readTitle(), searchQ),
+                    "Invalid search results for " + n.readTitle());
+        }
+        softAssert.assertAll();
+    }
+
+}
