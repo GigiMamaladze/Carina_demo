8654b5adfd0df813f29a4ccda1c74d39e874dafd
GigiMamaladze <gmamaladze@solvd.com>
2022-12-18 01:59:35 +0400

    created adBanner Class

---
+++ src/main/java/com/qaprosoft/carina/demo/gui/mytests/automation_excercise/components/AdBanner.java
@@ -0,0 +1,33 @@
+package com.qaprosoft.carina.demo.gui.mytests.automation_excercise.components;
+
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
+import com.qaprosoft.carina.core.gui.AbstractUIObject;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class AdBanner extends AbstractUIObject {
+    public AdBanner(WebDriver driver) {
+        super(driver);
+    }
+
+    public AdBanner(WebDriver driver, SearchContext searchContext) {
+        super(driver, searchContext);
+    }
+
+    @FindBy(xpath = "//*[@id='card']")
+    private ExtendedWebElement banner;
+
+    @FindBy(xpath = "//*[@id='dismiss-button']")
+    private ExtendedWebElement dismissButton;
+
+    public boolean isBannerPresent() {
+        return banner.isVisible();
+    }
+
+    public void clickDismissBtn() {
+        dismissButton.click();
+    }
+
+
+}
